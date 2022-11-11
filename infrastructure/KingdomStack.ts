03080373e24f5cda4cf086a8f7c10f0212e3ee80
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-11-11 13:34:51 +1100

    add lambda policy to list buckets

--- infrastructure/KingdomStack.ts
+++ infrastructure/KingdomStack.ts
@@ -10,6 +10,7 @@ import {
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { GenericTable } from "./GenericTable";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class KingdomStack extends Stack {
 	private api = new RestApi(this, "KingdomApi");
@@ -18,20 +19,20 @@ export class KingdomStack extends Stack {
 	constructor(scope?: Construct, id?: string, props?: StackProps) {
 		super(scope, id, props);
 
-		const helloLambda = new LambdaFunction(this, "helloLambda", {
-			runtime: Runtime.NODEJS_16_X,
-			code: Code.fromAsset(join(__dirname, "..", "services", "hello")),
-			handler: "hello.main",
-		});
-
 		const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
 			runtime: Runtime.NODEJS_16_X,
 			entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
 			handler: "handler",
 		});
 
+		const s3ListPolicy = new PolicyStatement();
+		s3ListPolicy.addActions("s3:ListAllMyBuckets");
+		s3ListPolicy.addResources("*");
+
+		helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
 		// Hello Api lambda integration
-		const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+		const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
 		const helloLambdaResource = this.api.root.addResource("hello");
 		helloLambdaResource.addMethod("GET", helloLambdaIntegration);
 	}
