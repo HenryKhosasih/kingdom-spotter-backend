21e3189f92f2407a7d1d31b9a5f5c421b7b4049c
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-11-15 09:15:16 +1100

    Create AuthorizerWrapper class and integrate Cognito Authorizer to sample Hello API

--- infrastructure/KingdomStack.ts
+++ infrastructure/KingdomStack.ts
@@ -1,11 +1,21 @@
 import { Construct } from "constructs";
 import { Stack, StackProps } from "aws-cdk-lib";
-
-import { RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+	AuthorizationType,
+	LambdaIntegration,
+	MethodOptions,
+	RestApi,
+} from "aws-cdk-lib/aws-apigateway";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
+import { join } from "node:path";
 import { GenericTable } from "./GenericTable";
+import { AuthorizerWrapper } from "./auth/AuthorizerWrapper";
 
 export class KingdomStack extends Stack {
 	private api = new RestApi(this, "KingdomApi");
+	private authorizer: AuthorizerWrapper;
+
 	private KingdomTable = new GenericTable(this, {
 		tableName: "KingdomTable",
 		primaryKey: "kingdomId",
@@ -18,6 +28,32 @@ export class KingdomStack extends Stack {
 
 	constructor(scope?: Construct, id?: string, props?: StackProps) {
 		super(scope, id, props);
+
+		this.authorizer = new AuthorizerWrapper(this, this.api);
+
+		const helloLambdaNodeJs = new NodejsFunction(this, "helloLambdaNodeJs", {
+			entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
+			handler: "handler",
+		});
+		const s3ListPolicy = new PolicyStatement();
+		s3ListPolicy.addActions("s3:ListAllMyBuckets");
+		s3ListPolicy.addResources("*");
+		helloLambdaNodeJs.addToRolePolicy(s3ListPolicy);
+
+		const optionsWithAuthorizer: MethodOptions = {
+			authorizationType: AuthorizationType.COGNITO,
+			authorizer: {
+				authorizerId: this.authorizer.authorizer.authorizerId,
+			},
+		};
+
+		// Hello API lambda integration:
+		const helloLambdaIntegration = new LambdaIntegration(helloLambdaNodeJs);
+		const helloLambdaResource = this.api.root.addResource("hello");
+		helloLambdaResource.addMethod(
+			"GET",
+			helloLambdaIntegration, optionsWithAuthorizer
+		);
 
 		// Kingdom API integrations
 		const kingdomResource = this.api.root.addResource("kingdom");
