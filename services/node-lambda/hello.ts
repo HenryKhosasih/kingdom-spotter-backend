656a4b9d8b51a1d9831038331714f7ac21933c96
Henry Khosasih <henrylaurentkhosasih@gmail.com>
2022-11-15 18:04:42 +1100

    Configure idp with group for finer access control

--- services/node-lambda/hello.ts
+++ services/node-lambda/hello.ts
@@ -1,15 +1,33 @@
+import { APIGatewayProxyEvent } from "aws-lambda";
 import { S3 } from "aws-sdk";
 
 const s3Client = new S3();
 
 async function handler(event: any, context: any) {
-	const buckets = await s3Client.listBuckets().promise();
 	console.log("Got an event:");
 	console.log(event);
-	return {
-		statusCode: 200,
-		body: "Here are your buckets: " + JSON.stringify(buckets.Buckets),
-	};
+
+	if (isAuthorized(event)) {
+		return {
+			statusCode: 200,
+			body: JSON.stringify("You are authorized as Admins"),
+		};
+	} else {
+		return {
+			statusCode: 401,
+			body: JSON.stringify("You are not authorized"),
+		};
+	}
+}
+
+function isAuthorized(event: APIGatewayProxyEvent) {
+	const groups = event.requestContext.authorizer?.claims["cognito:groups"];
+
+	if (groups) {
+		return (groups as string).includes("admins");
+	}
+
+	return false;
 }
 
 export { handler };
--- test/playground/hello.test.ts
+++ test/playground/hello.test.ts
@@ -4,8 +4,8 @@ import { handler } from "../../services/KingdomTable/Create";
 const event: APIGatewayProxyEvent = {
 	body: {
 		name: "testName",
-		location: "testLocation"
-	}
+		location: "testLocation",
+	},
 } as any;
 
 const result = handler(event, {} as any).then((apiResult) => {
